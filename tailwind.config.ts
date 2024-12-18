4e0ade09da81c9dfe4d5ae976f85ae41e7a5e78c
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-12-18 17:03:49 +0300

    chore: setup theme and initials of styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,33 @@ import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+	theme: {
+		fontFamily: {
+			sans: ['Commissioner', 'sans-serif']
+		},
+		extend: {
+			colors: {
+				base: {
+					100: '#1279EF',
+					200: '#1365C2',
+					300: '#1057A7',
+					content: '#FFFFFF',
+					cardBase: 'rgba(0,0,0, 0.2)'
+				},
+				secondary: '#FF6FB3',
+				accent: '#F9F871'
+			},
+			borderRadius: {
+				sm: '20px',
+				md: '24px',
+				lg: '28px',
+				xl: '32px',
+				'2xl': '40px'
+			}
+		}
+	},
 
-  plugins: [typography, containerQueries]
+	plugins: [typography, containerQueries]
 } satisfies Config;
