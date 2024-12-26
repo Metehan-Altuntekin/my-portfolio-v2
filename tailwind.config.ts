426e0e91673baf51abf8111eb99cbe50595724ef
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-12-27 01:31:51 +0300

    chore: whole lot of settings for responsive and so on

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,9 @@ export default {
 
 	theme: {
 		fontFamily: {
-			sans: ['Commissioner', 'sans-serif']
+			sans: ['Commissioner', 'Helvetica', 'sans-serif']
 		},
+
 		extend: {
 			colors: {
 				base: {
@@ -16,18 +17,27 @@ export default {
 					200: '#1365C2',
 					300: '#1057A7',
 					content: '#FFFFFF',
+					'content-muted': '#BED1EA',
 					cardBase: 'rgba(0,0,0, 0.2)'
 				},
 				secondary: '#FF6FB3',
 				accent: '#F9F871'
 			},
+
 			borderRadius: {
-				xs: '24px',
+				xs: '20px',
 				sm: '28px',
 				md: '32px',
 				lg: '36px',
-				xl: '40px',
-				'2xl': '44px'
+				xl: '42px',
+				'2xl': '48px'
+			},
+
+			fontSize: {},
+
+			screens: {
+				'3xl': '1710px',
+				'4xl': '1920px'
 			}
 		}
 	},
