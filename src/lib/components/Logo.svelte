d8e953e38200d6823c230665e283aa8946bfecc7
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-12-28 22:46:08 +0300

    chore: add hero pic and logo

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,10 @@
+<span class="logo text-xl sm:text-2xl md:text-3xl lg:text-4xl">Metehan Altuntekin</span>
+
+<style>
+	.logo {
+		font-weight: 700;
+		font-variation-settings:
+			'FLAR' 100,
+			'VOLM' 100;
+	}
+</style>
