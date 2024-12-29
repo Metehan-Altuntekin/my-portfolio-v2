710f840fd5a70a6c26392e5550c2f618f288f7d6
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-12-29 17:56:57 +0300

    fix: prevent missing id error by creating the missing sections
    to try to fix vercel error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,13 @@
 	import GridBg from '$lib/components/GridBG.svelte';
 	import Header from '$lib/sections/Header.svelte';
 	import Hero from '$lib/sections/Hero.svelte';
+	import Projects from '$lib/sections/Projects.svelte';
+	import Skills from '$lib/sections/Skills.svelte';
 </script>
 
 <GridBg />
 
 <Header />
 <Hero />
+<Projects />
+<Skills />
