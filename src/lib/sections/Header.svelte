823b22976a73f2b56b463b269d4d97f227cb7d33
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-12-29 20:30:09 +0300

    feat: header background with scroll

--- src/lib/sections/Header.svelte
+++ src/lib/sections/Header.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	import Logo from '$lib/components/Logo.svelte';
+
+	let scrollY = $state(0);
+	let scrolled = $derived(scrollY > 200);
+	$inspect(scrolled);
 </script>
 
-<header class="fixed left-0 right-0 top-0 w-full px-[var(--page-padding)] py-3 md:py-6">
+<svelte:window bind:scrollY />
+
+<header
+	class="z-50 fixed left-0 right-0 top-0 w-full px-[var(--page-padding)] py-3 md:py-6
+	{scrolled && 'bg-base-content/5 backdrop-blur-lg'}
+ "
+>
 	<Logo />
 </header>
