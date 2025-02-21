85fe76eb1068e097259f8697edc8f02b0030a6b1
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2025-02-21 17:35:21 +0300

    chore: add proper projects content

--- src/lib/content/projects.ts
+++ src/lib/content/projects.ts
@@ -1,4 +1,9 @@
-import allsetThumb from '$lib/content/thumbs/allset-thumb.webp';
+import allsetThumb from '$lib/content/thumbs/allset.webp';
+import breezbookThumb from '$lib/content/thumbs/breezbook.webp';
+import kurdanciogluThumb from '$lib/content/thumbs/kurdancioglu.webp';
+import ozkanMusavirlikThumb from '$lib/content/thumbs/ozkan-musavirlik.webp';
+import tojsonThumb from '$lib/content/thumbs/tojson.webp';
+import breezbookBookingThumb from '$lib/content/thumbs/breezbook-booking.webp';
 
 export const tools = {
 	ts: {
@@ -45,53 +50,54 @@ export const tools = {
 
 const projects: Project[] = [
 	{
-		name: 'Allset.chat',
-		desc: 'Allset.chat is a project I made the design and the frontend development of the landing site of.',
+		name: 'AllSet',
+		desc: 'allset.chat is a an AI receptionist that works through WhatsApp. I have designed the brand identity and the landing site.',
 		tools: [tools.ts, tools.figma, tools.svelte, tools.tailwind, tools.daisyui],
 		tags: ['Design', 'Frontend'],
 		visitUrl: 'https://allset.chat',
 		thumb: allsetThumb
 	},
 	{
-		name: 'Allset.chat',
-		desc: 'Allset.chat is a project I made the design and the frontend development of the landing site of.',
-		tools: [tools.ts],
+		name: 'BreezBook',
+		desc: 'BreezBook is the early version of AllSet that focused on being a website solution.',
+		tools: [tools.ts, tools.svelte, tools.daisyui, tools.tailwind],
 		tags: ['Design', 'Frontend'],
-		visitUrl: 'https://allset.chat',
-		thumb: allsetThumb
+		visitUrl: 'https://breezbook.com',
+		thumb: breezbookThumb
 	},
 	{
-		name: 'Allset.chat',
-		desc: 'Allset.chat is a project I made the design and the frontend development of the landing site of.',
-		tools: [tools.ts],
-		tags: ['Design', 'Frontend'],
-		visitUrl: 'https://allset.chat',
-		thumb: allsetThumb
+		name: 'BreezBook Booking App',
+		desc: "BreezBook mobile booking app design prototype made in Figma. Hasn't been coded but we are proud of it.",
+		tools: [tools.figma],
+		tags: ['Design'],
+		visitUrl:
+			'https://www.figma.com/proto/Y3429sNI1L6LkuI8Jy8h4b/Booking-App?page-id=6332%3A16614&node-id=7112-19778&p=f&viewport=385%2C457%2C0.09&t=S0bQVVQMdWIHw9NU-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=7112%3A19778',
+		thumb: breezbookBookingThumb
 	},
 	{
-		name: 'Allset.chat',
-		desc: 'Allset.chat is a project I made the design and the frontend development of the landing site of.',
-		tools: [tools.ts],
+		name: 'Ozkan Musavirlik',
+		desc: 'Ozkan Mali Musavirlik is a landing and blog site I designed and coded for my accountancy firm. ',
+		tools: [tools.ts, tools.svelte, tools.sass, tools.figma],
 		tags: ['Design', 'Frontend'],
-		visitUrl: 'https://allset.chat',
-		thumb: allsetThumb
+		visitUrl: 'https://www.ozkanmusavirlik.com.tr',
+		thumb: ozkanMusavirlikThumb
 	},
 	{
-		name: 'Allset.chat',
-		desc: 'Allset.chat is a project I made the design and the frontend development of the landing site of.',
-		tools: [tools.ts],
+		name: 'ToJson.dev',
+		desc: 'ToJson is a tool you can extract data from any kind of files like a PDF, image files etc. and integrate them with your other tools.',
+		tools: [tools.ts, tools.daisyui, tools.tailwind],
 		tags: ['Design', 'Frontend'],
 		visitUrl: 'https://allset.chat',
-		thumb: allsetThumb
-	},
-	{
-		name: 'Allset.chat',
-		desc: 'Allset.chat is a project I made the design and the frontend development of the landing site of.',
-		tools: [tools.ts],
-		tags: ['Design', 'Frontend'],
-		visitUrl: 'https://allset.chat',
-		thumb: allsetThumb
+		thumb: tojsonThumb
 	}
+	// {
+	// 	name: 'Kurdancioglu Ltd.',
+	// 	desc: 'Kurdancioglu Ltd. is a company I made up for fun and practicing. One of the earliest designs I made. ("Tree Flesh" wording is just for fun :D)',
+	// 	tools: [tools.sass],
+	// 	tags: ['Design', 'Frontend'],
+	// 	visitUrl: 'https://metehan-altuntekin.github.io/Kurdancioglu-Ltd',
+	// 	thumb: kurdanciogluThumb
+	// }
 ];
 
 export default projects;
