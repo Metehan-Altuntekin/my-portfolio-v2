04a1b5db0e48787e2362de83bc5ab09339af2d8e
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2025-07-26 01:25:25 +0300

    feat: remove note from footer

--- src/lib/sections/Footer.svelte
+++ src/lib/sections/Footer.svelte
@@ -6,5 +6,4 @@
 	class="w-full px-[var(--page-padding)] py-3 md:py-7 flex flex-col justify-center items-center border-t-2 border-white/30 border-dashed"
 >
 	<Logo />
-	<div class="text-subtitle text-xs mt-4">Personal portfolio site.</div>
 </header>
