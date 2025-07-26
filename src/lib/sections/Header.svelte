b5ea86cc823d6b747cbb27bb53f63c5a69091973
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2025-07-27 01:44:08 +0300

    feat: smoothen header bg change and reduce trigger range

--- src/lib/sections/Header.svelte
+++ src/lib/sections/Header.svelte
@@ -2,7 +2,7 @@
 	import Logo from '$lib/components/Logo.svelte';
 
 	let scrollY = $state(0);
-	let scrolled = $derived(scrollY > 200);
+	let scrolled = $derived(scrollY > 70);
 	$inspect(scrolled);
 </script>
 
@@ -10,6 +10,7 @@
 
 <header
 	class="z-50 fixed left-0 right-0 top-0 w-full px-[var(--page-padding)] py-3 md:py-5
+				transition-all duration-500
 				{scrolled && 'bg-base-content/5 backdrop-blur-lg'}	"
 >
 	<Logo />
