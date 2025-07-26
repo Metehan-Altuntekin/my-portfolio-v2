2382ac36163b609e7c51e149051c80f03eee4150
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2025-07-27 00:36:38 +0300

    fix: footer uses wrong element header

--- src/lib/sections/Footer.svelte
+++ src/lib/sections/Footer.svelte
@@ -2,8 +2,10 @@
 	import Logo from '$lib/components/Logo.svelte';
 </script>
 
-<header
-	class="w-full px-[var(--page-padding)] py-3 md:py-7 flex flex-col justify-center items-center border-t-2 border-white/30 border-dashed"
+<footer
+	class="w-full px-[var(--page-padding)] py-3 md:py-7
+				flex flex-col justify-center items-center
+				border-t-2 border-white/30 border-dashed"
 >
 	<Logo />
-</header>
+</footer>
