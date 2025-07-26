b98c6713e6a1a1e97e93899e535f33924fd71841
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2025-07-27 00:51:40 +0300

    feat: improved footer line and logo size

--- src/lib/sections/Footer.svelte
+++ src/lib/sections/Footer.svelte
@@ -3,9 +3,13 @@
 </script>
 
 <footer
-	class="w-full px-[var(--page-padding)] py-3 md:py-7
-				flex flex-col justify-center items-center
-				border-t-2 border-white/30 border-dashed"
+	class="w-full flex flex-col justify-center items-center
+	 			px-[var(--page-padding)] pb-4 gap-4
+				md:py-10 md:gap-10"
 >
+	<svg width="100%" height="1" viewBox="0 0 100% 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<line y1="0.5" x2="100%" y2="0.5" stroke="white" stroke-opacity="0.5" stroke-dasharray="8 8" />
+	</svg>
+
 	<Logo />
 </footer>
