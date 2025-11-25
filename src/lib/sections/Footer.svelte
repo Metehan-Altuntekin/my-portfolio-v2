07ea69bf149fe6b77ea27308ae8310562b72e65e
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2025-11-26 01:54:17 +0300

    feat: footer better styling - remove line and add margin

--- src/lib/sections/Footer.svelte
+++ src/lib/sections/Footer.svelte
@@ -5,11 +5,11 @@
 <footer
 	class="w-full flex flex-col justify-center items-center
 	 			px-(--page-padding) pb-4 gap-4
-				md:py-10 md:gap-10"
+				md:py-10 md:gap-10 lg:mt-24"
 >
-	<svg width="100%" height="1" viewBox="0 0 100% 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<!-- <svg width="100%" height="1" viewBox="0 0 100% 1" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<line y1="0.5" x2="100%" y2="0.5" stroke="white" stroke-opacity="0.5" stroke-dasharray="8 8" />
-	</svg>
+	</svg> -->
 
 	<Logo />
 </footer>
