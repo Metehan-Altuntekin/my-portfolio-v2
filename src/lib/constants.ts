63d3c9705b3ce484c3980f1d1bbc41b263a4b091
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2025-12-21 17:48:40 +0300

    refactor: remove unused constants in lib/constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,4 @@
 import { dev } from '$app/environment';
-export const title = "Your Website's Title";
-export const description = 'A description of your website.';
 export const BASE_URL = dev ? 'http://localhost:5173' : 'https://metehan.design';
 
 // Site metadata for SEO
