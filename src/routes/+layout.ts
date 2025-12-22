4c514abf1934c0b787dc4c4e323eb1104803b428
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2025-12-22 18:27:37 +0300

    feat: set up posthog

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,16 @@
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
+
 export const prerender = true;
+
+export const load = async () => {
+	if (browser) {
+		posthog.init('phc_1NmSk28YUeBJb4LI88avENb41KYFlAZM48bSR30kPsp', {
+			api_host: 'https://us.i.posthog.com',
+			defaults: '2025-11-30',
+			person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+		});
+	}
+
+	return;
+};
