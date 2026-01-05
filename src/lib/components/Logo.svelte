c46b3c89b04988300130c899a0f537ef08f126ac
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2026-01-05 17:55:15 +0300

    perf: move logo style to app.css to reduce render blocking css file

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,10 +1,1 @@
 <span class="logo text-lg md:text-xl 3xl:text2xl text-shadow">Metehan Altuntekin</span>
-
-<style>
-	.logo {
-		font-weight: 700;
-		font-variation-settings:
-			'FLAR' 100,
-			'VOLM' 100;
-	}
-</style>
