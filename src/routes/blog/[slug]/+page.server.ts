1e746a30ffce90e86dff7858bcc29836b502d2f1
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2026-01-05 17:46:11 +0300

    refactor: blog/slug entries list

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,15 +1,11 @@
-import { getPostsByLanguage } from '$lib/blog-utils';
+import { getAllPosts, getPostsByLanguage } from '$lib/blog-utils';
 
 export async function entries() {
 	// Get posts for both languages
-	const postsEn = await getPostsByLanguage('en');
-	const postsTr = await getPostsByLanguage('tr');
+	const { posts } = await getAllPosts();
 
 	// Return all slugs for prerendering
-	const slugs: { slug: string }[] = [
-		...postsEn.map((post) => ({ slug: post.slug })),
-		...postsTr.map((post) => ({ slug: post.slug }))
-	];
+	const slugs: { slug: string }[] = [...posts.map((post) => ({ slug: post.slug }))];
 
 	return slugs;
 }
