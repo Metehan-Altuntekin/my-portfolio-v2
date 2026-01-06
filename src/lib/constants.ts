029dcbbb1215268412f7a797e3111a419340a57f
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2026-01-06 17:29:02 +0300

    chore: update twitter handle

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,5 +5,5 @@ export const BASE_URL = dev ? 'http://localhost:5173' : 'https://metehan.design'
 export const SITE_NAME = 'Metehan Altuntekin';
 export const SITE_DESCRIPTION =
 	'Think, research, design, build. Full-Stack Designer & Developer engineering high-performance SvelteKit & React apps.';
-export const TWITTER_HANDLE = '@metehan_design'; // Update with your actual Twitter handle
+export const TWITTER_HANDLE = '@metelomaniac'; // Update with your actual Twitter handle
 export const AUTHOR_NAME = 'Metehan Altuntekin';
