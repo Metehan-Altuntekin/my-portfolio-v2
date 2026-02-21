eb83bdc3499ca719ffac9e49482fffc3cefbf54e
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2026-02-21 22:31:29 +0300

    Revert "perf: bundle small packages into one .js file"
    This reverts commit 2b37701f4175bc85a0efe84039349becb8e475c3.

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
 
-// to be split into their own chunks automatically
-const heavyLibs: string[] = []; // e.g. "three"
-
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
@@ -20,25 +17,6 @@ export default defineConfig({
 		})
 	],
 	build: {
-		target: 'esnext',
-		rollupOptions: {
-			output: {
-				// bundle small node_modules packages into one single .js file
-				// to reduce the amount of the small chunks to prevent extra network usage
-				manualChunks: (id) => {
-					if (id.includes('node_modules')) {
-						// check if it's a heavy lib
-						for (const lib of heavyLibs) {
-							if (id.includes(lib)) {
-								return lib;
-							}
-						}
-
-						// everything else goes to vendor
-						return 'vendor';
-					}
-				}
-			}
-		}
+		target: 'esnext'
 	}
 });
